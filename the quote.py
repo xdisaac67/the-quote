402d6500de461435e76fc80205836802fc764a55
xdisaac67 <isaacpenah0912@hotmail.com>
2026-04-09 15:06:42 -0400

    Update the quote.py

--- the quote.py
+++ the quote.py
@@ -1,10 +1,15 @@
 import flet as ft
 def main(page:ft.Page):
 
+#funtion
+    def change_text(e):
+        normal.value = language.value
+
 #text
     normal = ft.Text(value="What will u have after 500 years",size=20,font_family = "Nunito")
 #buttons
     language = ft.RadioGroup(
+        on_change = change_text,
     content=ft.Row(controls=[
         ft.Radio(value = "what will you have after 500 years!", label="english"),
         ft.Radio(value = "O que você terá daqui a 500 anos!", label="portuguese"),
