07b6fa4f6d2c24da0dab72940dc1f881c653649e
Isaac Pena <isaacpenah0912@hotmail.com>
2026-04-09 11:43:02 -0400

    Add files via upload

---
+++ the quote.py
@@ -0,0 +1,17 @@
+import flet as ft
+def main(page:ft.Page):
+
+#text
+    normal = ft.Text(value="What will u have after 500 years",size=20,font_family = "Nunito")
+#buttons
+    language = ft.RadioGroup(
+    content=ft.Row(controls=[
+        ft.Radio(value = "what will you have after 500 years!", label="english"),
+        ft.Radio(value = "O que você terá daqui a 500 anos!", label="portuguese"),
+        ft.Radio(value = "Que restera-t-il après 500 ans!", label= "french" ),
+        ft.Radio(value = "¿Qué tendrás dentro de 500 años", label = "spanish")]))
+    
+
+    page.add(language,normal)
+
+ft.run (main=main)
